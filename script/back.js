d49dc79a51ee48b8ef431e9a310af7e3e76215ff
ruhan <mdruhanahmed49@gmail.com>
2025-03-02 22:12:17 +0600

    card complete

---
+++ script/back.js
@@ -0,0 +1,4 @@
+// alert('5463')
+document.getElementById('back').addEventListener('click',function(){
+    window.location.href="./index.html"
+})
--- script/math.js
+++ script/math.js
@@ -1,4 +1,11 @@
-// alert('alert')
+document.getElementById('discover').addEventListener('click',function(){
+    window.location.href="./blogs.html"
+})
+document.getElementById('back').addEventListener('click',function(){
+    window.location.href="./index.html"
+})
+
+alert('42524')
 
 document.getElementById('shop-btn').addEventListener('click',function(event){
     alert('alert')
@@ -20,6 +27,160 @@ document.getElementById('shop-btn').addEventListener('click',function(event){
     const checkboxSum = conCkBx + number;
     const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
 
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Fix Mobile Button Issue  at 12:48:15 PM </p>
+    
+    
+    `
+    containar.appendChild(div);
+
+    document.getElementById("shop-btn").disabled = true;
+
+})
+
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
+})
+
+document.getElementById('dark-btn').addEventListener('click',function(event){
+    alert('alert')
+    event.preventDefault();
+    const task = document.getElementById('task').innerText;
+    const convertedtask =parseFloat(task);
+    
+    
+
+    const checkboxNumber = document.getElementById('checkbox-number').innerText;
+    const conCkBx=parseFloat(checkboxNumber);
 
 
+    const number = 1;
+    const tasksum = convertedtask - number;
+    const maintask = document.getElementById('task').innerText = tasksum;
+
+
+    const checkboxSum = conCkBx + number;
+    const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
+
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Task Add Dark Mode at 12:48:15 PM </p>
+    
+    
+    `
+    containar.appendChild(div);
+
+    document.getElementById("dark-btn").disabled = true;
+
+})
+
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
+})
+
+
+
+document.getElementById('page-btn').addEventListener('click',function(event){
+    alert('alert')
+    event.preventDefault();
+    const task = document.getElementById('task').innerText;
+    const convertedtask =parseFloat(task);
+    const checkboxNumber = document.getElementById('checkbox-number').innerText;
+    const conCkBx=parseFloat(checkboxNumber);
+    const number = 1;
+    const tasksum = convertedtask - number;
+    const maintask = document.getElementById('task').innerText = tasksum;
+    const checkboxSum = conCkBx + number;
+    const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Optimize  Home page at 12:48:15 PM </p>
+    
+    `
+    containar.appendChild(div);
+    document.getElementById("page-btn").disabled = true;
+})
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
+})
+
+
+
+document.getElementById('meta-btn').addEventListener('click',function(event){
+    alert('alert')
+    event.preventDefault();
+    const task = document.getElementById('task').innerText;
+    const convertedtask =parseFloat(task);
+    const checkboxNumber = document.getElementById('checkbox-number').innerText;
+    const conCkBx=parseFloat(checkboxNumber);
+    const number = 1;
+    const tasksum = convertedtask - number;
+    const maintask = document.getElementById('task').innerText = tasksum;
+    const checkboxSum = conCkBx + number;
+    const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Add new emoji at 12:48:15 PM </p>
+    
+    `
+    containar.appendChild(div);
+    document.getElementById("meta-btn").disabled = true;
+})
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
+})
+
+document.getElementById('google-btn').addEventListener('click',function(event){
+    alert('alert')
+    event.preventDefault();
+    const task = document.getElementById('task').innerText;
+    const convertedtask =parseFloat(task);
+    const checkboxNumber = document.getElementById('checkbox-number').innerText;
+    const conCkBx=parseFloat(checkboxNumber);
+    const number = 1;
+    const tasksum = convertedtask - number;
+    const maintask = document.getElementById('task').innerText = tasksum;
+    const checkboxSum = conCkBx + number;
+    const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Integrate OpenAI API at 12:48:15 PM </p>
+    
+    `
+    containar.appendChild(div);
+    document.getElementById("google-btn").disabled = true;
+})
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
+})
+
+document.getElementById('glass-btn').addEventListener('click',function(event){
+    alert('alert')
+    event.preventDefault();
+    const task = document.getElementById('task').innerText;
+    const convertedtask =parseFloat(task);
+    const checkboxNumber = document.getElementById('checkbox-number').innerText;
+    const conCkBx=parseFloat(checkboxNumber);
+    const number = 1;
+    const tasksum = convertedtask - number;
+    const maintask = document.getElementById('task').innerText = tasksum;
+    const checkboxSum = conCkBx + number;
+    const mainCkBx = document.getElementById('checkbox-number').innerText = checkboxSum;
+    const containar = document.getElementById('History');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>You have Complete The Improve Job searching at 12:48:15 PM </p>
+    
+    `
+    containar.appendChild(div);
+    document.getElementById("glass-btn").disabled = true;
+})
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('History').style.display = 'none';
 })
